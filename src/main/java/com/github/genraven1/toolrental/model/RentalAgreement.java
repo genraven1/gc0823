213df7fcc5b512939ea69d53d02e659cf18032b7
genraven1 <genraven1@gmail.com>
2023-08-11 17:00:32 -0500

    Update Rental Agreement Logic

--- src/main/java/com/github/genraven1/toolrental/model/RentalAgreement.java
+++ src/main/java/com/github/genraven1/toolrental/model/RentalAgreement.java
@@ -2,18 +2,47 @@ package com.github.genraven1.toolrental.model;
 
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 public class RentalAgreement {
 
+    public RentalAgreement(final Tool tool, final Checkout checkout) {
+        this.tool = tool;
+        this.checkout = checkout;
+    }
+
     private Tool tool;
 
-    private int days;
-
-    private Date checkout;
+    private Checkout checkout;
 
     public void printRentalAgreement() {
-
+        final StringBuilder builder = new StringBuilder();
+        // Print Tool Code
+        builder.append("Tool Code: ").append(this.tool.getCode()).append(System.getProperty("line.separator"));
+        // Print Tool Type
+        builder.append("Tool Type: ").append(this.tool.getType().getLabel()).append(System.getProperty("line.separator"));
+        // Print Tool Brand
+        builder.append("Tool Brand: ").append(this.tool.getBrand()).append(System.getProperty("line.separator"));
+        // Print Rental Days
+        builder.append("Rental Days: ").append(this.checkout.getDays()).append(System.getProperty("line.separator"));
+        // Print Checkout Date
+        builder.append("Check out date: ").append(this.checkout.getCheckout()).append(System.getProperty("line.separator"));
+        // Print Due Date
+        final LocalDate dueDate = this.checkout.getCheckout().plusDays(this.checkout.getDays());
+        builder.append("Due date: ").append(dueDate).append(System.getProperty("line.separator"));
+        // Print Daily Rental Charge
+        builder.append("Daily Rental Charge: ").append(this.tool.getType().getCharge()).append(System.getProperty("line.separator"));
+        // Print Charge Days
+        builder.append("Charge days: ").append("").append(System.getProperty("line.separator"));
+        // Print Pre-Discount Charge
+        builder.append("Pre-discount charge: ").append("").append(System.getProperty("line.separator"));
+        // Print Discount Percent
+        builder.append("Discount percent: ").append("").append(System.getProperty("line.separator"));
+        // Print Discount Amount
+        builder.append("Discount amount: ").append("").append(System.getProperty("line.separator"));
+        // Print Final Charge
+        builder.append("Final charge: ").append("").append(System.getProperty("line.separator"));
+        System.out.println(builder);
     }
 }
--- src/main/java/com/github/genraven1/toolrental/service/ToolService.java
+++ src/main/java/com/github/genraven1/toolrental/service/ToolService.java
@@ -2,8 +2,12 @@ package com.github.genraven1.toolrental.service;
 
 import com.github.genraven1.toolrental.model.Checkout;
 import com.github.genraven1.toolrental.model.RentalAgreement;
+import com.github.genraven1.toolrental.model.Tool;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @NoArgsConstructor
@@ -11,10 +15,33 @@ public class ToolService {
 
     /**
      * Generates a Rental Agreement for the Tool being checked out.
+     *
      * @param checkout The information about the checkout for the tool.
      * @return The Rental Agreement for that tool and checkout
      */
     public RentalAgreement generateRentalAgreement(final Checkout checkout) {
-        return new RentalAgreement();
+        final RentalAgreement agreement = new RentalAgreement(getToolByCode(checkout.getCode()), checkout);
+        agreement.printRentalAgreement();
+        return agreement;
+    }
+
+    public List<Tool> getAllTools() {
+        return Arrays.asList(
+                new Tool("CHNS", Tool.Type.CHAINSAW, Tool.Brand.STIHL),
+                new Tool("LADW", Tool.Type.LADDER, Tool.Brand.WERNER),
+                new Tool("JAKD", Tool.Type.JACKHAMMER, Tool.Brand.DEWALT),
+                new Tool("JAKR", Tool.Type.JACKHAMMER, Tool.Brand.RIDGID)
+        );
+    }
+    
+    public Tool getToolByCode(final String code) {
+        final List<Tool> tools = getAllTools();
+        Tool foundTool = null;
+        for (final Tool tool : tools) {
+            if (tool.getCode().equals(code)) {
+                foundTool = tool;
+            }
+        }
+        return foundTool;
     }
 }
